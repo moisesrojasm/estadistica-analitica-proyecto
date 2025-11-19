68a9c49c7316764da0f3d53b86c8bc5d4d3d8786
Diego Moisés Rojas Mata <147342399+moisesrojasm@users.noreply.github.com>
2025-11-18 21:54:27 -0600

    Refactorizar comentarios

--- main.R
+++ main.R
@@ -4,20 +4,19 @@
 
 rm(list = ls())
 
-# Cargar librerías (aunque aún no las uses)
+# Cargar librerías
 # library(here)
 
 # Cargar funciones (archivos vacíos)
-# Estas líneas no fallarán si los archivos existen aunque estén vacíos
+
 source("Funciones/Función_1.R")
 source("Funciones/Función_2.R")
 source("Funciones/Función_3.R")
 
-# Leer datos (archivo vacío o inexistente no debe romper el script hoy)
-# Crea un vector de ejemplo por ahora
+# Leer datos 
+# Crea un vector de ejemplo
 mis_datos <- c(10, 20, 30, 40, 50)
 
-# Imprimir algo mínimo para comprobar que corre
 print("Estructura del proyecto cargada correctamente.")
 print(mis_datos)
 
